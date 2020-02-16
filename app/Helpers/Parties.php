b66c49e2a1122eb4e8b33cf7024a16802e38d6db
norictech <ahmad.uji1902@gmail.com>
2020-02-16 21:27:47 +0700

    single sign-on

--- app/Helpers/Parties.php
+++ app/Helpers/Parties.php
@@ -2,11 +2,26 @@
 
 namespace App\Helpers;
 
+use GuzzleHttp\Client as Guzzle;
+
 class Parties {
+
+    public static function guzzle() {
+        return new Guzzle();
+    }
 
     public static function all() {
         $parties = \App\Parties::all();
         return response()->json($parties)->getContent();
+    }
+
+    public static function auth($method = 'POST', $action, $data = NULL, $data_key_mode = 'form_params', $special_condition = []) {
+        $parties = json_decode(static::all());
+        foreach ($parties as $party_key => $party) {
+            $http_response[$party_key] = \App\Helpers\Parties::guzzle()->request($method, $party->app_url . $action, [
+                $data_key_mode => $data
+            ]);
+        }
     }
 
 }
--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -36,5 +37,35 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        $parties_login = \App\Helpers\Parties::auth('POST', 'parties/login', $request->toArray(), 'form_params');
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (
+            method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)
+        ) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
     }
 }
