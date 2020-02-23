29b24490df9c3d87eff2498d5bbddb6745093855
norictech <ahmad.uji1902@gmail.com>
2020-02-23 20:18:35 +0700

    user role relation

--- app/Role.php
+++ app/Role.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
@@ -9,4 +10,8 @@ class Role extends Model
     protected $fillable = [
         'role_name', 'status', 'description'
     ];
+
+    public function users() {
+        return $this->hasMany(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Role;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -37,4 +38,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function roles() {
+        return $this->belongsTo(Role::class);
+    }
 }
